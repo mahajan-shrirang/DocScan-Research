1de962b26bb9b54f6d7e8c1e6e490ab0da3703cb
Biyani-Dhananjay <dhananjay.biyani@ellicium.com>
2024-07-10 15:55:31 +0530

    feat: merge coco files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utils.preprocess import extract_images
+from utils.merge import find_paths, merge_results, copy_images
 import fitz
 import os
 
@@ -12,6 +13,22 @@ def main():
     for i in range(45,92):
         output_path = os.path.join(output_folder, f"output_folder{i + 1}.png")
         extract_images(input_pdf, i, output_path)
+        
+    base_paths = [
+        r'D:\Data Science\DocScan-Research\output\coco files\set 1',
+        r'D:\Data Science\DocScan-Research\output\coco files\set 2',
+        r'D:\Data Science\DocScan-Research\output\coco files\set 3',
+        r'D:\Data Science\DocScan-Research\output\coco files\set 4'
+    ]
+
+    result_json_paths = find_paths("result.json", base_paths)
+    output_path = r"D:\Data Science\DocScan-Research\output\annotated\result.json"
+    if not os.path.exists(os.path.dirname(output_path)):
+        os.makedirs(os.path.dirname(output_path))
+    output_images_folder = r"D:\Data Science\DocScan-Research\output\annotated\images"
+
+    merge_results(result_json_paths, output_path)
+    copy_images(base_paths, output_images_folder)
         
 if __name__ == "__main__":
     main()
---
+++ utils/merge.py
@@ -0,0 +1,61 @@
+import os
+import json
+import shutil
+
+def find_paths(file_name, base_paths):
+    file_paths = []
+    for path in base_paths:
+        for root, dirs, files in os.walk(path):
+            if file_name in files:
+                file_path = os.path.join(root, file_name)
+                file_paths.append(file_path)
+    return file_paths
+
+def merge_results(paths, output_path):
+    combined_coco = {
+        'images': [],
+        'annotations': [],
+        'categories': []
+    }
+
+    image_id_offset = 0
+    annotation_id_offset = 0
+    image_id_mapping = {}
+
+    for path in paths:
+        with open(path, 'r') as f:
+            coco_data = json.load(f)
+
+            if not combined_coco['categories']:
+                combined_coco['categories'] = coco_data['categories']
+            
+            for image in coco_data['images']:
+                new_image_id = image['id'] + image_id_offset
+                image_id_mapping[image['id']] = new_image_id
+                image['id'] = new_image_id
+                combined_coco['images'].append(image)
+            
+            for annotation in coco_data['annotations']:
+                new_annotation_id = annotation['id'] + annotation_id_offset
+                new_image_id = image_id_mapping[annotation['image_id']]
+                annotation['id'] = new_annotation_id
+                annotation['image_id'] = new_image_id
+                combined_coco['annotations'].append(annotation)
+            
+            image_id_offset += len(coco_data['images'])
+            annotation_id_offset += len(coco_data['annotations'])
+
+    with open(output_path, 'w') as f:
+        f.writelines(json.dumps(combined_coco, indent=4))
+
+def copy_images(base_paths, output_images_folder):
+    if not os.path.exists(output_images_folder):
+        os.makedirs(output_images_folder)
+
+    for path in base_paths:
+        images_folder = os.path.join(path, 'Images')
+        for image_filename in os.listdir(images_folder):
+            src_image_path = os.path.join(images_folder, image_filename)
+            dst_image_path = os.path.join(output_images_folder, image_filename)
+            shutil.copy(src_image_path, dst_image_path)
+
