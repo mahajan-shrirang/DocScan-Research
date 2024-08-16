93823395151b227b96513638b715cd7636859463
Biyani-Dhananjay <dhananjay.biyani@ellicium.com>
2024-08-16 15:14:27 +0530

    feat: add data preview . fix: UI alignment

--- app.py
+++ app.py
@@ -1,81 +1,116 @@
-from core.inference import inference_image
-from utils.merge import merge_images_and_save_pdf
-from utils.preprocess import preprocess_pdf
-from utils.postprocess import get_summary
-
-import sqlite3
-import fitz
+import os
+import pandas as pd
+import requests
 import streamlit as st
 import io
-import os
-import datetime
+import fitz
 from PIL import Image
-import numpy as np
-import pandas as pd
-import cv2
-import time
-from zipfile import ZipFile
+from utils.preprocess import preprocess_pdf
 
 st.set_page_config(page_title="DocScan", page_icon="📄", layout="wide")
 
+BASEURL = "http://localhost:8000"
+
+@st.experimental_dialog("Are you sure?")
+def are_you_sure(selected_job):
+    print(type(selected_job))
+    st.header("Are you sure you want to kill job: " + str(selected_job))
+    try:
+        if st.button("Yes"):
+            response = kill_job(selected_job)
+            st.write(response)
+            st.rerun()
+    except ValueError as e:
+        st.error(e)
+
+def upload_file(file, model):
+    url = BASEURL + ("/inference/" if model == "YOLOv4" else "/inference-detr/")
+    files = {"file": (file.name, file.getvalue(), "application/pdf")}
+    response = requests.post(url, files=files)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise ValueError(response.json())
+
+def get_processes():
+    url = BASEURL + "/jobs/"
+    response = requests.get(url)
+    return pd.DataFrame(response.json()["processes"])
+
+def kill_job(job_id):
+    url = BASEURL + "/kill/" + str(job_id) + "/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise ValueError(response.text)
+
+def download_file(job_id):
+    url = BASEURL + "/get-output/" + str(job_id) + "/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    else:
+        raise FileNotFoundError(response.text)
+    
+def display_pdf_preview(file):
+    images = fitz.open(stream=file.read(), filetype="pdf")
+    page = images.load_page(1)
+    image = page.get_pixmap()
+    img = Image.open(io.BytesIO(image.tobytes("png")))
+    st.image(img, caption=f"Page 1", use_column_width=False , width=500)
+    
 st.title("DocScan")
 
 select_model = st.sidebar.selectbox(
     "Select model", ("YoLo", "Detr"),
-    index=None,
     placeholder="Select a model"
-    )
+)
 
 file = st.sidebar.file_uploader("Upload a file", type=["pdf"])
-os.makedirs("results", exist_ok=True)
 
-conn = sqlite3.connect('data.db')
-query = "SELECT id, job_id, status, timestamp FROM processes"
-all_data = pd.read_sql(query, conn)
-st.table(all_data if not all_data.empty else "No data available")
-conn.close()
+if file is not None:
+    st.toast("File uploaded successfully",icon="🎉")
+    if st.sidebar.button("Process"):
+        display_pdf_preview(file)
+        model = "YOLOv4" if select_model == "YoLo" else "DETR"
+        try:
+            response = upload_file(file, model)
+            st.write(response)
+        except ValueError as e:
+            st.toast(e)
+            
+tab = st.tabs(["Get Processes"])[0]
 
-if file:
-    if not file.name.endswith(".pdf"):
-        st.error("Invalid file format. Please upload a PDF file.")
-    if st.button("Process"):
-        file_obj = file.getvalue()
-        directory_name = f"{str(file.name).split('.')[0]}-\
-                {datetime.datetime.now()}".replace(" ", "_").replace(":", "-").replace(".", "-")
-        directory_name = "results/" + directory_name
-        os.makedirs(directory_name, exist_ok=True)
-        with open(f"{directory_name}/input.pdf", "wb") as f:
-            f.write(file_obj)
-        os.makedirs(f"{directory_name}/output", exist_ok=True)
-        output_dir = f"{directory_name}/output"
-        images: list[fitz.Pixmap] = preprocess_pdf(file_obj)
-        if len(images) == 0:
-            st.error("No images found in the PDF file.")
-        output_images = []
-        results = []
-        start_time = time.time()
-        for image in images:
-            byte_code = image.tobytes()
-            image: Image = Image.open(io.BytesIO(byte_code))
-            image: cv2.Mat = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-            output_image, boxes = inference_image(image)
-            output_images.append(output_image)
-            results.append(boxes)
-        end_time = time.time()
-        print(f"Time taken: {end_time - start_time} seconds")
-        st.write(f"Time taken: {end_time - start_time} seconds")
-        merge_images_and_save_pdf(output_images,
-                                  save_path=f"{output_dir}/output.pdf")
-        if len(results[0]) == 0:
-            st.error("No objects detected in the PDF file.")
-        results = get_summary(results)
-        results.index += 1
-        results.to_csv(f"{output_dir}/summary.csv", index_label="page no.")
-        print(f"Results saved to `{output_dir}/summary.csv`")
-        print(f"Output saved to `{output_dir}/output.pdf`")
-        with ZipFile(f"{output_dir}/output.zip", 'w') as zipf:
-            zipf.write(f"{output_dir}/output.pdf", "output.pdf")
-            zipf.write(f"{output_dir}/summary.csv", "summary.csv")
-        output_file = io.open(os.path.join(output_dir, "output.zip"), "rb")
-        output_bytes = output_file.read()
-        st.download_button("Download output", output_bytes, "output.zip", "application/zip")
+if tab:
+    jobs = get_processes()
+    refresh = st.button("🔄️")
+    display_pdf_preview(file)
+    if refresh:
+        jobs = get_processes()
+        if jobs.shape[0] > 0:
+            jobs['Timestamp'] = pd.to_datetime(jobs["Timestamp"])
+
+    if jobs.shape[0] != 0:
+        event = st.dataframe(jobs, on_select='rerun', hide_index=True, selection_mode="single-row")
+
+        if len(event.selection['rows']):
+            selected_row = event.selection['rows'][0]
+            job_id = jobs.iloc[selected_row]['job_id']
+            status = jobs.iloc[selected_row]['status']
+            Timestamp = jobs.iloc[selected_row]['Timestamp']
+
+            st.session_state['selected_job'] = job_id
+
+            if status not in ["Completed", "Failed", "Killed"]:
+                kill = st.button("❌ Kill Job")
+                if kill:
+                    are_you_sure(job_id)
+
+            if status == "Completed":
+                try:
+                    st.download_button("Download Output Zip", download_file(job_id), "output.zip", "application/zip")
+                except FileNotFoundError as e:
+                    st.error(e)
+    else:
+        st.dataframe(pd.DataFrame(columns=["ID", "job_id", "status", "Timestamp"]))
